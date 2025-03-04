169228fdd797fbab2222aa43957889b03166b66c
ilyab <ilya.bubnoff@yandex.ru>
2025-03-05 00:24:35 +0300

    Did my homework

--- src/Author.java
+++ src/Author.java
@@ -1,4 +1,21 @@
 public class Author {
     private String firctName;
     private String lastName;
+
+    public Author(String firctName, String lastName) {
+        this.firctName = firctName;
+        this.lastName = lastName;
+    }
+
+    public String getFirctName() {
+        return this.firctName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+    @Override
+    public String toString() {
+        return firctName + " " + lastName;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Book auther = new Book();
-        book.name =
+        Author dumas = new Author("Александр", "Дюма");
+        Author dreiser = new Author("Теодор", "Драйзер");
+        Book countOfMonteCristo = new Book("Граф Монте-Кристо", dumas, 1844);
+        Book theFinancier = new Book("Финансист", dreiser, 1912);
+        System.out.println("Название - " + countOfMonteCristo.getTitle());
+        System.out.println("Автор - " + countOfMonteCristo.getAuthor());
+        System.out.println("Год публикации - " + countOfMonteCristo.getPublicationYear());
+        countOfMonteCristo.setPublicationYear(1846);
+        System.out.println("Изменённый год публикации - " + countOfMonteCristo.getPublicationYear());
+        System.out.println("Название - " + theFinancier.getTitle());
+        System.out.println("Автор - " + theFinancier.getAuthor());
+        System.out.println("Год публикации - " + theFinancier.getPublicationYear());
     }
 }
